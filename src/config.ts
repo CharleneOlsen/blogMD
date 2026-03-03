afdd66fa3fe96029dac1af8abee0b07bee351712
Dejakova <dejakovm@vscht.cz>
2026-03-03 13:28:16 +0100

    changing the name and timezone

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  author: "Milana Dejaková",
+  profile: "https://milanadejakova.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "Hello",
   ogImage: "astropaper-og.jpg",
@@ -19,5 +19,5 @@ export const SITE = {
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Europe/Prague", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
